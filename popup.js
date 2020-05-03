82cf159b31a89e74d9a8c107697a0b8f37e58ac3
peiqi liu <leo20021210@163.com>
2020-05-03 14:38:32 +0800

    first commit

---
+++ popup.js
@@ -0,0 +1,15 @@
+
+var a=function(){
+  chrome.tabs.update({url: "https://www.facebook.com/peiqi.liu.332"});
+};
+document.getElementById('facebook').onclick=a;
+
+var b=function(){
+  chrome.tabs.update({url: "https://twitter.com/LEO21309113"});
+};
+document.getElementById('twitter').onclick=b;
+
+var c=function(){
+  chrome.tabs.update({url: "https://user.qzone.qq.com/3356968394?source=namecardhoverqzone"});
+};
+document.getElementById('QQ').onclick=c;
